7f588d8dfcef2d16fe7caa9e689df767833fb800
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-03-05 22:38:07 +0800

    clean code

--- Sources/AlfredOnePee/Workflow.swift
+++ Sources/AlfredOnePee/Workflow.swift
@@ -1,7 +1,39 @@
 import Foundation
 
 
+struct Login: Codable {
+    
+    let title: String
+    let url: String
+    let realUrl: String?
+    let match: String?
+    let icon: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title, url
+        case realUrl = "real_url"
+        case match, icon
+    }
+
+}
+
+
+enum LoginType {
+    
+    case curated
+    case custom
+
+}
+
+
 public struct Workflow {
+    
+    static let alfredPreferences = ProcessInfo.processInfo.environment["alfred_preferences"]
+    static let alfredWorkflowUID = ProcessInfo.processInfo.environment["alfred_workflow_uid"]
+    
+    static let sortLoginsBy = ProcessInfo.processInfo.environment["sort_by"] ?? "sort_by_usage"
+    static let customLoginsPath = ProcessInfo.processInfo.environment["custom_logins_path"]
+    
     
     public static func next() -> String {
         ProcessInfo.processInfo.environment["next"] ?? "oops"
